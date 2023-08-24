bff5fe6dae4d8dbd0e09b60738afaedccea227d1
Josh Schonstal <josh@schonstal.org>
2023-08-24 15:39:55 -0700

    Improve color gradient

--- ingot/ingot.gd
+++ ingot/ingot.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
   if Input.is_action_just_pressed("next"):
     temp = 0
     
-  graphic.modulate = 1.5 * gradient_texture.gradient.sample(temp/100.0)
+  graphic.modulate = 1.5 * gradient_texture.gradient.sample(clamp(temp - 5, 0, 90)/100.0)
 
 func heat(amount:float) -> void:
   temp += amount
