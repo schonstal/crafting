1f7d7a061623a7763ffbaa7ec6193638d1cb2f07
Josh Schonstal <josh@schonstal.org>
2023-08-25 17:20:10 -0700

    UI and Imports

--- UI/combo/active_combo.gd
+++ UI/combo/active_combo.gd
@@ -3,7 +3,10 @@ extends Control
 var combo:Combo
 
 @export var item_height := 100.0
+
 @onready var nine_patch_rect: NinePatchRect = $NinePatchRect
+@onready var instructions: Control = $Instructions
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var tween:Tween
 
@@ -17,6 +20,8 @@ func change_combo(new_combo: Combo):
   nine_patch_rect.size.y = 52
   nine_patch_rect.position.y = -52
   
+  instructions.size.y = 0
+  
   var new_size = Vector2(
     nine_patch_rect.size.x,
     combo.steps.size() * item_height + 100
@@ -26,12 +31,19 @@ func change_combo(new_combo: Combo):
     40
   )
   
+  var instructions_size = Vector2(instructions.size.x, new_size.y)
+  
   if tween != null:
     tween.stop()
   tween = create_tween()
   
-  tween.set_ease(Tween.EASE_OUT)
-  tween.set_trans(Tween.TRANS_QUART)
-  tween.tween_property(nine_patch_rect,"position", new_position, 0.2)
-  tween.set_trans(Tween.TRANS_ELASTIC)
-  tween.tween_property(nine_patch_rect, "size", new_size, 0.3)
+  tween.set_ease(Tween.EASE_OUT)\
+       .set_trans(Tween.TRANS_QUART)\
+       .tween_property(nine_patch_rect,"position", new_position, 0.2)
+  
+  tween.chain()\
+       .set_trans(Tween.TRANS_ELASTIC)\
+       .tween_property(nine_patch_rect, "size", new_size, 0.3)
+  
+  tween.parallel()\
+       .tween_property(instructions, "size", instructions_size, 0.3)
